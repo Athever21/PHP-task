6523d22225a87fe6fdbd8032ce599c1b403ef5b8
Athever21 <ogrek.pawe25@gmail.com>
2026-04-01 20:56:45 +0200

    feat: product page finished

--- public/router.php
+++ public/router.php
@@ -1,5 +1,11 @@
 <?php
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+ini_set('log_errors', '1');
+ini_set('error_log', 'php://stderr');
+error_reporting(E_ALL);
+
 if (php_sapi_name() === 'cli-server') {
     $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
     $file = __DIR__ . $path;
